3379d5677e816e37f3ed500367065628e09b2201
Evelyn Nguyen <enguyen6208@gmail.com>
2022-07-26 22:22:07 -0700

    started styling profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Airtable from "airtable";
-import './Profile.css';
+import "./Profile.css";
 
 const base = new Airtable({ apiKey: "keycVUqNgXbPQmTBb" }).base(
   "apppAm9jBXoifxazs"
@@ -31,40 +31,69 @@ const Profile = () => {
     <>
       {selectSpecies !== null && (
         <div className="profilePage">
-          <h1>{selectSpecies.fields["Botanical Name"]}</h1>
-          <h2>{selectSpecies.fields["Family Botanical Name"]}</h2>
-          <h2>{selectSpecies.fields["Other Known Names"]}</h2>
-          <p>{selectSpecies.fields["Tree Profile"]}</p>
-          <p>{selectSpecies.fields["Plant or Tree"]}</p>
-          {selectSpecies.fields["Products and Uses"] !== undefined
-            ? selectSpecies.fields["Products and Uses"].map((product) => (
-                <p>{product}</p>
+          <h1 className="pageHeader">
+            {selectSpecies.fields["Botanical Name"]}
+          </h1>
+          <div className="plantNames">
+            <p className="indent">
+              <b>Family Botanical Name </b>
+            </p>
+            {selectSpecies.fields["Family Botanical Name"]}
+          </div>
+          <div className="plantNames">
+            <p className="indent">
+              <b>Common Names </b>
+            </p>
+            {selectSpecies.fields["Other Known Names"]}
+          </div>
+          <div className="plantNames">
+            <p className="indent">
+              <b>Plant or Tree </b>
+            </p>
+            {selectSpecies.fields["Plant or Tree"]}
+          </div>
+          <div className="plantNames">
+            <p className="indent">
+              <b>Products and Uses</b>
+            </p>
+            {selectSpecies.fields["Products and Uses"] !== undefined
+              ? selectSpecies.fields["Products and Uses"].map((product) => (
+                  <p className="productNames">{product}</p>
+                ))
+              : null}
+          </div>
+          <h4>Botanical Description</h4>
+          <p className="description">{selectSpecies.fields["Tree Profile"]}</p>
+          {selectSpecies.fields["Image of tree"] !== undefined
+            ? selectSpecies.fields["Image of tree"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
               ))
             : null}
-          {selectSpecies.fields["Image of tree"] !== undefined
-            ? <img src={selectSpecies.fields["Image of tree"][0].thumbnails.large.url}></img>
-            : null
-          }
           {selectSpecies.fields["Image of Flower"] !== undefined
-            ? <img src={selectSpecies.fields["Image of Flower"][0].thumbnails.large.url}></img>
-            : null
-          }
+            ? selectSpecies.fields["Image of Flower"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
+              ))
+            : null}
           {selectSpecies.fields["Image of fruit"] !== undefined
-            ? <img src={selectSpecies.fields["Image of fruit"][0].thumbnails.large.url}></img>
-            : null
-          }
+            ? selectSpecies.fields["Image of fruit"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
+              ))
+            : null}
           {selectSpecies.fields["Image of Seed"] !== undefined
-            ? <img src={selectSpecies.fields["Image of Seed"][0].thumbnails.large.url}></img>
-            : null
-          }
+            ? selectSpecies.fields["Image of Seed"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
+              ))
+            : null}
           {selectSpecies.fields["Image of Leaf"] !== undefined
-            ? <img src={selectSpecies.fields["Image of Leaf"][0].thumbnails.large.url}></img>
-            : null
-          }
+            ? selectSpecies.fields["Image of Leaf"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
+              ))
+            : null}
           {selectSpecies.fields["Illustration"] !== undefined
-            ? <img src={selectSpecies.fields["Illustration"][0].thumbnails.large.url}></img>
-            : null
-          }
+            ? selectSpecies.fields["Illustration"].map((image) => (
+                <img src={image.thumbnails.large.url}></img>
+              ))
+            : null}
         </div>
       )}
     </>
